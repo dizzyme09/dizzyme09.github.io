7c2898316a56303b28c9dcdf2adb615ca5f40074
Belvin Shandy Aurora <belshandy1209@gmail.com>
2022-09-02 21:08:39 +0700

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/dizzyme09/dizzyme09.github.io'
+      className='link footer__link'
+    >
+      Created By Dizzyme09
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,45 @@
+const header = {
+  homepage: 'https://dizzyme09.github.io',
+  title: 'Dizzyme09',
+}
+
+const about = {
+  name: 'Belvin',
+  description:
+    'an Informatics Engineering Student. And currently interested in AI and Machine Learning.',
+  social: {
+    linkedin: 'https://linkedin.com/in/belshandy',
+    github: 'https://github.com/dizzyme09',
+    instagram: 'https://instagram.com/vin.bel',
+  },
+}
+
+const projects = [
+  {
+    name: 'CRUD App with express and mongodb',
+    description:
+      'A simple CRUD app with ExpressJS and MongoDB. This app is about a student database.',
+    stack: ['NodeJS', 'MongoDB', 'ExpressJS'],
+    sourceCode: 'https://github.com/dizzyme09/CRUD-expressJS',
+  },
+  {
+    name: 'CRUD App with php and mysql',
+    description:
+      'A simple CRUD app with PHP and MySQL. This is my first project with PHP and MySQL for my college final project.',
+    stack: ['PHP', 'MySQL', 'React'],
+    sourceCode: 'https://github.com/peminjaman-buku',
+  },
+  {
+    name: 'Website App with Django',
+    description:
+      'A website application about a simulation of build your own PC (PC Builder look alike). This is my first project with Django for my college final project.',
+    stack: ['Django'],
+    sourceCode: 'https://github.com/dizzyme09/PC-Builder-Simulator',
+  },
+]
+
+const contact = {
+  email: 'belshandy1209@mail.com',
+}
+
+export { header, about, projects, contact }
